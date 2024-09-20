2a01d587b634f08947b089b2bc504538cf592084
Tauseeb Khan <khantauseeb2026@gmail.com>
2024-09-20 23:31:15 +0530

    Initial commit

---
+++ src/app/movie/movie.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-movie',
+  standalone: true,
+  imports: [],
+  templateUrl: './movie.component.html',
+  styleUrl: './movie.component.less'
+})
+export class MovieComponent {
+
+}
